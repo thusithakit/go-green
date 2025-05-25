2c74e52da333a6f065118a6d3c8db816ef8a009d
Thusithakit <thusithakit3@gmail.com>
2025-05-25 10:40:05 +0530

    added user-location pin

--- components/BinMap.tsx
+++ components/BinMap.tsx
@@ -4,7 +4,8 @@ import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { onValue, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 const mapContainerStyle = { width: "100%", height: "100%" };
-const center = { lat: 6.9271, lng: 79.8612 };
+let center = { lat: 6.9271, lng: 79.8612 };
+let userLocation: { lat: number; lng: number; } | null = null;
 interface Bin {
     id: string;
     lat: number;
@@ -33,6 +34,26 @@ const levelMarker = (level: number) => {
     return imgUrl;
 }
 
+const getUserLocation = ()=>{
+    console.log("center coordinates",center)
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                const {latitude, longitude} = position.coords;
+                center ={lat:latitude,lng:longitude}
+                userLocation ={lat:latitude,lng:longitude}
+                console.log("center coordinates",center)
+            },
+            (error)=>{
+                console.error("Error getting user location",error);
+            }
+        );
+    }
+    else{
+        console.error('Geolocation is not supported by this browser.');
+    }
+}
+
 const BinMap = () => {
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
@@ -54,12 +75,21 @@ const BinMap = () => {
                 setBins([]);
             }
         });
+        getUserLocation();
         return () => unsubscribe();
     }, []);
     if (!isLoaded) return <div>Loading...</div>;
     return (
         <div className="relative h-[100vh] pt-[70px]">
             <GoogleMap mapContainerStyle={mapContainerStyle} zoom={14} center={center}>
+                {userLocation && (
+                    <Marker position={{ lat: userLocation.lat, lng: userLocation.lng }}
+                        icon={{
+                            url:"/user-pin.svg",
+                            scaledSize: new window.google.maps.Size(30, 30),
+                        }}
+                    />
+                )}
                 {bins.map((bin) => (
                     <Marker key={bin.id} position={{ lat: bin.lat, lng: bin.lng }}
                         icon={{
