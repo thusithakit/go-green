723b572d8e7cf1a9504518182c7f7903a4f83e59
Thusitha Kithuldora <74904988+thusithakit@users.noreply.github.com>
2025-08-15 09:00:20 +0530

    fixed build errors

--- components/WebPushSetup.tsx
+++ components/WebPushSetup.tsx
@@ -98,7 +98,7 @@ function InstallPrompt() {
  
   useEffect(() => {
     setIsIOS(
-      /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+      /iPad|iPhone|iPod/.test(navigator.userAgent) && !('MSStream' in window)
     )
  
     setIsStandalone(window.matchMedia('(display-mode: standalone)').matches)
@@ -119,7 +119,7 @@ function InstallPrompt() {
             {' '}
             ⎋{' '}
           </span>
-          and then "Add to Home Screen"
+          and then &quot;Add to Home Screen&quot;
           <span role="img" aria-label="plus icon">
             {' '}
             ➕{' '}
